f6090155f57b7cd57674a24495987b799f9f1149
wine <mr.savior@ya.ru>
2022-07-21 17:41:59 +0300

    Brain-even realized

---
+++ bin/brain-even.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+
+const game = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ').trim();
+  console.log(`Hello, ${name}!`);
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  let userLost = false;
+
+  for (let i = 1; i <= 3; i += 1) {
+    const number = Math.ceil(Math.random() * 1000);
+    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ').trim();
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      userLost = true;
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      break;
+    }
+  }
+  console.log(userLost ? `Let's try again, ${name}!` : `Congratulations, ${name}!`);
+};
+
+game();
