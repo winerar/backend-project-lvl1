1351f7bd334ee75fd07928bab9a55a34fceb2119
wine <mr.savior@ya.ru>
2022-07-22 13:44:47 +0300

    Brain-gcd realized

--- bin/games/brain-gcd.js
+++ bin/games/brain-gcd.js
@@ -2,7 +2,7 @@
 import * as brain from '../../src/index.js';
 
 const gcd = (number1, number2) => {
-  if (number2 === 0) {
+  if (number2 > 0) {
     return gcd(number2, number1 % number2);
   }
   return number1;
@@ -18,8 +18,8 @@ const playGCDGame = () => {
   let playerLost = false;
 
   for (let i = 1; i <= brain.maxRoundsCount; i += 1) {
-    const number1 = brain.getRandomNumber(101);
-    const number2 = brain.getRandomNumber(101);
+    const number1 = brain.getRandomNumber(10);
+    const number2 = brain.getRandomNumber(10);
 
     const correctAnswer = gcd(number1, number2);
 
