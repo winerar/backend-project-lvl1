3d0147846858bd014dbec7b222971c4e28f83466
wine <mr.savior@ya.ru>
2022-07-22 11:02:39 +0300

    brain-utils.js realized

---
+++ src/brain-utils.js
@@ -0,0 +1,37 @@
+import readlineSync from 'readline-sync';
+
+export const showGreeting = () => {
+  console.log('Welcome to the Brain Games!');
+};
+
+export const getPlayerName = () => {
+  const name = readlineSync.question('May I have your name? ').trim();
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+
+export const checkPlayerAnswer = (correctAnswer) => {
+  const playerAnswer = readlineSync.question('Your answer: ').trim();
+  if (playerAnswer === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  return false;
+};
+
+const showCompassion = (playerName) => {
+  console.log(`Let's try again, ${playerName}!`);
+};
+
+const showCongratulation = (playerName) => {
+  console.log(`Congratulations, ${playerName}!`);
+};
+
+export const showResult = (playerName, playerLost) => {
+  if (playerLost) {
+    showCompassion(playerName);
+  } else {
+    showCongratulation(playerName);
+  }
+};
