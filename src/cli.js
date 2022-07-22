1fe223e08946c25be6539c99a30421713fd2f0ce
wine <mr.savior@ya.ru>
2022-07-22 09:51:03 +0300

    Function names fixed

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+export const showGreeting = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
 
-export default greeting;
+export default showGreeting;
