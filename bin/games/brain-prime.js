63c6953645c624fd7cda866c6dc2c824a40f3cf2
wine <mr.savior@ya.ru>
2022-07-22 19:22:06 +0300

    brain-prime realized

--- bin/games/brain-prime.js
+++ bin/games/brain-prime.js
@@ -1,1 +1,29 @@
 #!/usr/bin/env node
+import * as brain from '../../src/index.js';
+
+const isPrime = (number) => {
+  const limit = Math.sqrt(number);
+  for (let i = 2; i <= limit; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getQuestionAndAnswer = () => {
+  const question = 2 + brain.getRandomNumber(999);
+  const answer = isPrime(question) ? 'yes' : 'no';
+  const result = {
+    question,
+    answer,
+  };
+  return result;
+};
+
+const playPrimeGame = () => {
+  const task = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+  brain.startGame(task, getQuestionAndAnswer);
+};
+
+playPrimeGame();
