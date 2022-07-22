37b782efb5aadc5c1451415439847ffbe18b7128
wine <mr.savior@ya.ru>
2022-07-22 13:08:56 +0300

    Functions moved

--- src/index.js
+++ src/index.js
@@ -2,32 +2,36 @@ import readlineSync from 'readline-sync';
 
 export const maxRoundsCount = 3;
 
+export const showMessage = (message) => console.log(message);
+
+export const getRandomNumber = (maxNumber) => Math.floor(Math.random() * maxNumber);
+
 export const showGreeting = () => {
-  console.log('Welcome to the Brain Games!');
+  showMessage('Welcome to the Brain Games!');
 };
 
 export const getPlayerName = () => {
   const name = readlineSync.question('May I have your name? ').trim() || '%username%';
-  console.log(`Hello, ${name}!`);
+  showMessage(`Hello, ${name}!`);
   return name;
 };
 
 export const checkPlayerAnswer = (correctAnswer) => {
   const playerAnswer = readlineSync.question('Your answer: ').trim();
   if (playerAnswer === correctAnswer) {
-    console.log('Correct!');
+    showMessage('Correct!');
     return true;
   }
-  console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+  showMessage(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
   return false;
 };
 
 const showCompassion = (playerName) => {
-  console.log(`Let's try again, ${playerName}!`);
+  showMessage(`Let's try again, ${playerName}!`);
 };
 
 const showCongratulation = (playerName) => {
-  console.log(`Congratulations, ${playerName}!`);
+  showMessage(`Congratulations, ${playerName}!`);
 };
 
 export const showResult = (playerName, playerLost) => {
@@ -38,6 +42,3 @@ export const showResult = (playerName, playerLost) => {
   }
 };
 
-export const showTask = (task) => console.log(task);
-
-export const getRandomNumber = (maxNumber) => Math.floor(Math.random() * maxNumber);
